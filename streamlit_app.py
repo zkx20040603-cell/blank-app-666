806a110af30cf4c6c13cf3b71fb7a9d20692abd7
zkx20040603-cell <zkx20040603@gmail.com>
2025-11-21 16:30:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+https://api.open-meteo.com/v1/forecast?latitude=37.5665&longitude=126.9780&daily=temperature_2m_mean,relativehumidity_2m_mean,precipitation_sum&timezone=Asia/Seoul&start_date=2023-01-01&end_date=2023-12-31&format=csv
